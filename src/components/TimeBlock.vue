5eb87bb85afb6104a984a851b28d26bdc2a6bc91
ramazan.girfanov <ramazan.girfanov@tatar.ru>
2025-06-11 09:09:45 +0300

    v2

---
+++ src/components/TimeBlock.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  title: String,
+  subtitle: String,
+  imgSrc: String,
+})
+</script>
+
+<template>
+  <div class="time-block">
+    <div>
+      <img :src="imgSrc" alt="">
+    </div>
+    <div>
+      {{ title }}
+
+      <div class="time-block__br"></div>
+
+      {{ subtitle }}
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
