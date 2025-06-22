0a19cc91cd0bfb7185c3ec57a8924640d16adf43
ramazan.girfanov <ramazan.girfanov@tatar.ru>
2025-06-22 21:44:33 +0300

    bb

--- src/components/TimeBlock.vue
+++ src/components/TimeBlock.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <div class="time-block" style="display: flex;align-items: center; margin-bottom: 30px">
     <div>
-      <img :src="'happy/' + imgSrc" alt="" style="width: 100px;">
+      <img :src="'/happy/' + imgSrc" alt="" style="width: 100px;">
     </div>
     <div style="text-align: center; flex-grow: 1">
       {{ title }}
